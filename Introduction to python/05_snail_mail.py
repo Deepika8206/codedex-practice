caab3e9402606fb49dc009a7d33fd38f2c04963b
Deepika8206 <deepika.papitha78@gmail.com>
2025-06-27 22:36:14 +0530

    ..

---
+++ Introduction to python/05_snail_mail.py
@@ -0,0 +1,6 @@
+print(' 27th June 2025')
+print( " How am I feeling tdy : I'm feeling pretty good today!")
+print( " What I wanna accomplish by learning coding : I wanna learn how to code , even if it takes a lot of time, I want to learn and become better at it")
+print(" A message to my future programmer self would be: Take one thing at a time and You can definitely do it")
+print( ' My favourite Emoji is: Smiley ig')
+
