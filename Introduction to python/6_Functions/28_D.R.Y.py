7f0e4bc5fe094609a6d1673141dcacf7a64098ad
Deepika8206 <deepika.papitha78@gmail.com>
2025-08-26 17:01:03 +0530

    ..

---
+++ Introduction to python/6_Functions/28_D.R.Y.py
@@ -0,0 +1,8 @@
+print("print is a built-in function")
+minimum = min(4,5)
+print(minimum)
+maximum = max(6,7)
+print(maximum)
+input('Is input a built-in function?')
+seasons = ['Spring', 'Summer', 'Fall', 'Winter']
+print(list(enumerate(seasons)))
